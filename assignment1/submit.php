cbd3cd45be9e0a599315ad39ed43a3dc92b1a8f1
Jeremy T. Porter <tanner@lowestmed.com>
2017-01-31 09:10:07 -0700

    SMTP php.ini

--- assignment1/submit.php
+++ assignment1/submit.php
@@ -21,8 +21,8 @@
 
 // xampp code fix      
 ini_set( 'sendmail_from', "jtporter9@gmail.com" );
-// ini_set( 'SMTP', "	smtp-relay.gmail.com" );
-// ini_set( 'smtp_port', 25 );
+ini_set( 'SMTP', "	smtp-relay.gmail.com" );
+    ini_set( 'smtp_port', 25 );
 
 //BUILD THE EMAIL
 
